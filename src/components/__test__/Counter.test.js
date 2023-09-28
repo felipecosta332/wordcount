9c006fa5bd1b3f20ccd6c157343f1ab3afca1f22
Felipe Costa <felipecosta776@outlook.com>
2023-09-27 21:49:12 -0300

    test component rendering

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -1,8 +1,23 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { Counter } from "../Counter";
 
-test("render the textarea", () => {
-  const { screen } = render(<Counter />);
-  const textArea = screen.getByPlaceholderText("Type or paste your test");
-  expect(textArea).toBeTruthy();
+describe("Counter Component Test", () => {
+  test("render the textarea", () => {
+    render(<Counter />);
+    const textArea = screen.getByPlaceholderText("Type or paste your text");
+    expect(textArea).toBeTruthy();
+  });
+
+  test("render the character result", () => {
+    render(<Counter />);
+    const charLength = screen.getByTestId("charLength");
+    expect(charLength.innerHTML).toBe("Character: 0");
+  });
+
+  test("render the word result", () => {
+    render(<Counter />);
+    const wordLength = screen.getByText("Word: 0");
+    expect(wordLength).toBeInTheDocument();
+  });
 });
